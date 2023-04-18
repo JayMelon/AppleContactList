7c05d447b8ee68d416410da7930ea0bedda00813
Jalen Lewis <jaylewis0618@gmail.com>
2023-04-17 23:55:22 -0500

    Map Demo works

---
+++ ContactApp/LocationDemoController.swift
@@ -0,0 +1,139 @@
+//
+//  LocationDemoController.swift
+//  ContactApp
+//
+//  Created by user238354 on 4/17/23.
+//
+
+import UIKit
+import CoreLocation
+class LocationDemoController: UIViewController, CLLocationManagerDelegate {
+
+    @IBOutlet weak var txtAddress: UITextField!
+    @IBOutlet weak var txtCity: UITextField!
+    
+    @IBOutlet weak var txtZip: UITextField!
+    
+    
+    
+    @IBOutlet weak var lblLat: UILabel!
+    
+    @IBOutlet weak var lblLong: UILabel!
+    
+    @IBOutlet weak var lblAccuracy: UILabel!
+    
+    
+    @IBOutlet weak var lblHeading: UILabel!
+    
+    @IBOutlet weak var lblHeadingAccuracy: UILabel!
+    
+    
+    @IBOutlet weak var lblAlt: UILabel!
+    
+    @IBOutlet weak var lblAltAcc: UILabel!
+    
+    
+    @IBAction func addressToCords(_ sender: UIButton) {
+        let address = "\(txtAddress.text!), \(txtCity.text!), \(txtCity.text!))"
+        geoCoder.geocodeAddressString(address){
+            (placemarks, error) in
+            self.processAddressResponse(withPlacemarks: placemarks, error: error)
+        }
+    }
+    private func processAddressResponse(withPlacemarks placemarks: [CLPlacemark]?, error: Error?){
+        if let error = error {
+            print("Geocode error: \(error)")
+        }
+        else{
+            var bestMatch: CLLocation?
+            if let placemarks = placemarks,placemarks.count > 0 {
+                bestMatch = placemarks.first?.location
+            }
+            if let coordinate = bestMatch?.coordinate {
+                lblLat.text = String(format: "%g\u{00B0}",coordinate.latitude)
+                lblLong.text = String(format: "%g\u{00B0}",coordinate.longitude)
+            }
+            else {
+                print("Didn;t find anything")
+            }
+        }
+    }
+    
+    @IBAction func deviceCoords(_ sender: UIButton) {
+        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        locationManager.distanceFilter = 100
+        locationManager.startUpdatingLocation()
+        locationManager.startUpdatingHeading()
+    }
+    override func viewDidDisappear(_ animated: Bool) {
+        locationManager.stopUpdatingLocation()
+        locationManager.stopUpdatingHeading()
+    }
+    lazy var geoCoder = CLGeocoder()
+    var locationManager: CLLocationManager!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        // Do any additional setup after loading the view.
+    }
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last{
+            let eventDate = location.timestamp
+            let howRecent = eventDate.timeIntervalSinceNow
+            if Double(howRecent) < 15.0 {
+                let coordinate = location.coordinate
+                lblLat.text = String(format: "%g\u{00B0}",coordinate.latitude)
+                lblLong.text = String(format: "%g\u{00B0}",coordinate.longitude)
+                lblAccuracy.text = String(format: "%gm",location.horizontalAccuracy)
+                lblAlt.text = String(format: "%gm",location.altitude)
+                lblAltAcc.text = String(format: "%gm",location.verticalAccuracy)
+
+            }
+        }
+    }
+    func locationManager(_ _manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedWhenInUse {
+            print("Granted")
+        }else {
+            print("Denied")
+        }
+    }
+    func locationManager(_ manager: CLLocationManager, didUpdateHeading newHeading: CLHeading) {
+        if newHeading.headingAccuracy > 0 {
+            let theHeading = newHeading.trueHeading
+            var direction: String
+            switch theHeading {
+            case 225..<315:
+                direction = "W"
+            case 135..<225:
+                direction = "S"
+            case 45..<135:
+                direction = "E"
+            default:
+                direction = "N"
+            }
+            lblHeading.text = String(format: "%g\u{00B0} {%@}", theHeading, direction)
+            lblHeadingAccuracy.text = String(format: "%g\u{00B0}", newHeading.headingAccuracy)
+        }
+    }
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        let errorType = error._code == CLError.denied.rawValue ? "Location Permission Denied" : "Unknow Error"
+        let alertController = UIAlertController(
+            title: "Error Getting Location: \(errorType)", message: "Error message: \(error)", preferredStyle: .alert)
+        let actionOK = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alertController.addAction(actionOK)
+        present(alertController, animated: true, completion: nil)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
